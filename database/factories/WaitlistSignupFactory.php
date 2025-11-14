bfca19654dddd119b7c1e480ce37ead1bc72f2b7
Sabri Taieb <sabritaiebdev@gmail.com>
2025-11-14 18:23:59 +0100

    feat: add `WaitlistSignup` model and `waitlist_signups` DB table
    - Add a unit test
    - Add a migration script
    - Add a factory

---
+++ database/factories/WaitlistSignupFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\WaitlistSignup>
+ */
+final class WaitlistSignupFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'email' => $this->faker->email(),
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'welcome_email_sent_at' => null,
+        ];
+    }
+
+    public function welcomeEmailSentAt(?Carbon $date = null): self
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => $date ?? $this->faker->dateTimeBetween('-2 years'),
+        ]);
+    }
+}
