d883cf30960cb78b79e7869fc482403d54f2aeec
Sabri Taieb <sabritaiebdev@gmail.com>
2025-11-17 11:12:36 +0100

    feat: add Pagination vue component

---
+++ resources/js/components/Pagination.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { Link } from '@inertiajs/vue3'
+
+defineProps({
+    meta: {
+        type: Object,
+        required: true,
+    }
+})
+
+</script>
+
+<template>
+    <nav class="relative flex justify-center">
+        <template v-for="link in meta.links" :key="link.label">
+            <Link
+                preserve-scroll
+                :href="link.url ?? ''"
+                v-html="link.label"
+                class="flex items-center justify-center px-3 py-2 text-sm rounded-lg text-gray-600"
+                :class="{ 'bg-gray-200': link.active, '!text-gray-300': !link.url }"
+            />
+        </template>
+    </nav>
+</template>
