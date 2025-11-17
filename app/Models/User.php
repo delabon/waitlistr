615e055ac7bcbc9cc25c793cf89d19ba33614b92
Sabri Taieb <sabritaiebdev@gmail.com>
2025-11-17 17:13:02 +0100

    refactor: remove two factor code

--- app/Models/User.php
+++ app/Models/User.php
@@ -32,8 +32,6 @@ final class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'two_factor_secret',
-        'two_factor_recovery_codes',
         'remember_token',
     ];
 
