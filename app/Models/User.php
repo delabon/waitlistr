95d6e63297f1e791a47271f17d7a72829fc843d5
Sabri Taieb <sabritaiebdev@gmail.com>
2025-12-15 07:17:56 +0100

    refactor: add property annotations to User model class

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,22 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Enums\UserRole;
+use Carbon\CarbonInterface;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 
+/**
+ * @property-read int $id
+ * @property-read string $name
+ * @property-read string $email
+ * @property-read string $role
+ * @property-read CarbonInterface|null $email_verified_at
+ * @property-read string $password
+ * @property-read string|null $remember_token
+ * @property-read CarbonInterface $created_at
+ * @property-read CarbonInterface|null $updated_at
+ */
 final class User extends Authenticatable
 {
     use Notifiable, TwoFactorAuthenticatable;
