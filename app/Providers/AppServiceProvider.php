edc707a123b231bc5cb03f1914aa9e74c6d70695
Sabri Taieb <sabritaiebdev@gmail.com>
2025-12-15 07:28:15 +0100

    refactor: force HTTPS in production and prevent accessing to missing model attributes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 final class AppServiceProvider extends ServiceProvider
@@ -22,8 +24,12 @@ final class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        URL::forceHttps(
+            $this->app->isProduction()
+        );
         DB::prohibitDestructiveCommands(
             $this->app->isProduction(),
         );
+        Model::preventAccessingMissingAttributes();
     }
 }
