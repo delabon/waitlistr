11bdda48ecf4c88bc74b44beb34e03919b062096
Sabri Taieb <sabritaiebdev@gmail.com>
2026-07-05 08:48:18 +0100

    refactor: PaginateWaitlistSignupsAction class

--- app/Http/Controllers/WaitlistSignupController.php
+++ app/Http/Controllers/WaitlistSignupController.php
@@ -18,7 +18,7 @@ final class WaitlistSignupController extends Controller
 
     public function index(PaginateWaitlistSignupsAction $action): Response
     {
-        $waitlistSignups = $action(self::MAX_ITEMS_PER_PAGE);
+        $waitlistSignups = $action->handle(self::MAX_ITEMS_PER_PAGE);
 
         return Inertia::render('dashboard/admin/Signups', [
             'waitlistSignups' => WaitlistSignupResource::collection($waitlistSignups),
